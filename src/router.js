d6644673f4b3e2ecc9bdda474610ea6f7fd75204
mvorot <mikevorot@gmail.com>
2024-10-22 11:29:32 -0400

    Created empty files that need to be written

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AddFood from './components/AddFood.vue'
 import UserLogin from './components/Login.vue'
+// import ClaimFood from './components/ClaimFood.vue'
+// import ShowPantry from './components/ShowPantry.vue'
 
 const routes = [
+    // { path: '/', component: ShowPantry },
     { path: '/add', component: AddFood },
-    { path: '/login', component: UserLogin }
+    { path: '/login', component: UserLogin },
+    // { path: '/claim', component: ClaimFood }
 ]
 
 const router = createRouter({
